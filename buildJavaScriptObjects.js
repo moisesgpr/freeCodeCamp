653a48aceb6402c4da0c0bae209bd3ce40a2ee50
moisesgpr <moisesgp95@gmail.com>
2022-11-22 03:57:31 -0300

    Aprendiendo a programar

---
+++ buildJavaScriptObjects.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+  "name": "Dante",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Moises", "Natalia", "Noah"]
+  
+    // Only change code above this line
+  };
